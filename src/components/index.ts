2d18f6870b6c7eb451f6155f943dc9de3aadb915
Evgenia-cyber <65081637+Evgenia-cyber@users.noreply.github.com>
2022-08-11 12:18:55 +0000

    Deploying to gh-pages from @ Evgenia-cyber/dice@9a351a4cbe30ff4ee8fcfc7ca33354c8a6c82cf1 🚀

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as DiceWithAnimation } from "./Dice";
-export { default as Dice } from "./RandomDiceFace";
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-const validateRandomNumber = (randomNumber: number): number => {
-  if (randomNumber < 1) return 1;
-  if (randomNumber > 6) return 6;
-  return randomNumber;
-};
-
-export { validateRandomNumber };
